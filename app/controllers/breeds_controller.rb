554bf15eda354fe91c623af0c758048b1e4e0d48
asamonte40 <asamonte40@academic.rrc.ca>
2025-11-07 00:42:48 -0600

    controller

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,7 +1,10 @@
 class BreedsController < ApplicationController
   def index
+    @breeds = Breed.order(:name).page(params[:page]).per(10)
   end
 
   def show
+    @breed = Breed.find(params[:id])
+    @dogs = @breed.dogs
   end
 end
