eed4ced73e846e3363c70f1a7ba529ddc03093d7
asamonte40 <asamonte40@academic.rrc.ca>
2025-11-09 10:03:46 -0600

    troubleshooting for show.html

--- test/controllers/breeds_controller_test.rb
+++ test/controllers/breeds_controller_test.rb
@@ -2,12 +2,19 @@ require "test_helper"
 
 class BreedsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get breeds_index_url
+    get breeds_url
     assert_response :success
   end
 
   test "should get show" do
-    get breeds_show_url
-    assert_response :success
+    breed = Breed.create!(
+    name: "Test Breed",
+    description: "A sample test breed.",
+    temperament: "Friendly",
+    life_span: "10-12 years",
+    image_url: "https://placehold.co/300x300?text=Test+Breed"
+  )
+  get breed_url(breed)
+  assert_response :success
   end
 end
